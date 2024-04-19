0c3c1768ab39db31fbbceab61d4d7fa21c18ff4a
Bruk Teshome <brukteshome21@gmail.com>
2024-04-19 18:57:04 +0300

    add fund info struct

--- contracts/fundRaiser.sol
+++ contracts/fundRaiser.sol
@@ -2,4 +2,15 @@
 pragma solidity ^0.8.0;
 
 contract FundRaised {
+
+   struct fundInfo {
+        address beneficiar;
+        uint goal;
+        uint timeDuration;
+        bool isGoal;
+        bool isTimeDuration;
+        uint totalRaised;
+        mapping(address=>uint) fundRaised;
+        address[] fundersList;
+    }
 }

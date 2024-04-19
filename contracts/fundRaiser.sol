28c978367a01dc375f415c5b2370ba3d9c71589d
Bruk Teshome <brukteshome21@gmail.com>
2024-04-19 19:00:02 +0300

    Add events

--- contracts/fundRaiser.sol
+++ contracts/fundRaiser.sol
@@ -2,19 +2,31 @@
 pragma solidity ^0.8.0;
 
 contract FundRaised {
-  
-  struct fundInfo {
+    struct fundInfo {
         address fundRaiserCreator;
         uint goal;
         uint timeDuration;
         bool isGoal;
         bool isTimeDuration;
         uint totalRaised;
-        mapping(address=>uint) fundRaised;
+        mapping(address => uint) fundRaised;
         address[] fundersList;
     }
 
-  address public owner;
-  mapping(string=>fundInfo) public fundRaiseds;
-  string[] public fundNames; 
+    address public owner;
+    mapping(string => fundInfo) public fundRaiseds;
+    string[] public fundNames;
+
+    event _isGoal(uint _totalraised, uint _isGoal);
+    event _fund(
+        string indexed _fundName,
+        address _address,
+        uint indexed _amount
+    );
+    event _withdraw(
+        string indexed _fundName,
+        address _address,
+        uint indexed _amount
+    );
+    event _refund(address indexed _address, uint indexed _amount);
 }

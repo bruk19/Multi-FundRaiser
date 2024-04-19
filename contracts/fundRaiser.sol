4c88215d74ee44a9c7e3498b02f910a82bfdd70b
Bruk Teshome <brukteshome21@gmail.com>
2024-04-19 18:59:15 +0300

    Add state variables

--- contracts/fundRaiser.sol
+++ contracts/fundRaiser.sol
@@ -2,9 +2,9 @@
 pragma solidity ^0.8.0;
 
 contract FundRaised {
-
-   struct fundInfo {
-        address beneficiar;
+  
+  struct fundInfo {
+        address fundRaiserCreator;
         uint goal;
         uint timeDuration;
         bool isGoal;
@@ -13,4 +13,8 @@ contract FundRaised {
         mapping(address=>uint) fundRaised;
         address[] fundersList;
     }
+
+  address public owner;
+  mapping(string=>fundInfo) public fundRaiseds;
+  string[] public fundNames; 
 }

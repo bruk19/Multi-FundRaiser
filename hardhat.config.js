c0d0d8aefc511d47fba089978e6825fab8c5e1a9
Bruk Teshome <brukteshome21@gmail.com>
2024-04-20 10:03:39 +0300

    add required changes

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,7 +7,7 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY;
 // const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 module.exports = {
-  solidity: "0.8.0",
+  solidity: "0.8.24",
   networks: {
     sepolia: {
       url: SEPOLIA_RPC_URL,

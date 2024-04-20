9a2abaf98245e30d562abf9ced7e81ce49c28f99
Bruk Teshome <brukteshome21@gmail.com>
2024-04-20 08:59:24 +0300

    Add ethers dependencies

---
+++ contracts/deployment/deploy.js
@@ -0,0 +1,17 @@
+const hre = require("hardhat");
+
+async function main() {
+  const FundRaised = await hre.ethers.getContractFactory("FundRaised");
+  const fundRaised = await FundRaised.deploy();
+
+  await fundRaised.deployed();
+
+  console.log("FundRaised contract deployed to:", fundRaised.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });

08f8ce07c3a718a810eacdb14c547f1b155ad7e9
Bruk Teshome <brukteshome21@gmail.com>
2024-04-22 11:02:07 +0300

    Add a refiy function that verify the contract

--- deployment/deploy.js
+++ deployment/deploy.js
@@ -1,4 +1,5 @@
-const {ethers, run} = require("hardhat");
+const { ethers, run, network } = require("hardhat");
+
 
 async function main() {
   const FundRaised = await ethers.getContractFactory("FundRaised");
@@ -9,6 +10,25 @@ async function main() {
   console.log("FundRaised contract deployed to:", fundRaised.address);
 
   console.log(network.config)
+
+
+  }
+  async function verify(contractAddress, args) {
+    console.log("verify contract.....")
+    try {
+      await run("verify:verify", {
+        address: contractAddress,
+        constructorArguments: args,
+      })
+    } catch (e) {
+      if (e.message.toLowerCase().includes("already verified")) {
+        console.log("Already Verified")
+      }
+      else {
+        console.log(e)
+      }
+    }
+  }
 }
 
 
